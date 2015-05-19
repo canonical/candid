8c0817839c50f4487c8a1730419340591d804341
Fabrice Matrat <fabricematrat@gmail.com>
2015-05-19 13:48:59 +0200

    Separate test file.

--- internal/v1/gravatar_test.go
+++ internal/v1/gravatar_test.go
@@ -1,7 +1,9 @@
-package v1
+package v1_test
 
 import (
 	gc "gopkg.in/check.v1"
+
+	"github.com/CanonicalLtd/blues-identity/internal/v1"
 )
 
 type gravatarSuite struct{}
@@ -9,8 +11,8 @@ type gravatarSuite struct{}
 var _ = gc.Suite(&gravatarSuite{})
 
 func (*gravatarSuite) TestGravatarHash(c *gc.C) {
-	c.Assert(gravatarHash("myemail@domain.com"), gc.Equals, gravatarHash("myemail@domain.com "))
-	c.Assert(gravatarHash("myemail@domain.com"), gc.Equals, gravatarHash(" myemail@domain.com"))
-	c.Assert(gravatarHash("myemail@domain.com"), gc.Equals, gravatarHash("MYEMAIL@domain.com"))
-	c.Assert(gravatarHash("jbloggs3@example.com"), gc.Equals, "21e89fe03e3a3cc553933f99eb442d94")
+	c.Assert(v1.GravatarHash("myemail@domain.com"), gc.Equals, v1.GravatarHash("myemail@domain.com "))
+	c.Assert(v1.GravatarHash("myemail@domain.com"), gc.Equals, v1.GravatarHash(" myemail@domain.com"))
+	c.Assert(v1.GravatarHash("myemail@domain.com"), gc.Equals, v1.GravatarHash("MYEMAIL@domain.com"))
+	c.Assert(v1.GravatarHash("jbloggs3@example.com"), gc.Equals, "21e89fe03e3a3cc553933f99eb442d94")
 }
