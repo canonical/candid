a3cbc1ce4cbf46269659112a6602b6737d63c0ab
Martin Hilton <martin.hilton@canonical.com>
2016-12-13 07:59:47 +0000

    cmd/user-admin: support find with last-login and last-discharge time
    Update the QueryUsers endpoint to support search for users by last-login
    and last-discharge times and add support to user-admin to query using
    those parameters with the find command.

--- cmd/user-admin/internal/admincmd/find.go
+++ cmd/user-admin/internal/admincmd/find.go
@@ -3,14 +3,22 @@
 package admincmd
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/juju/cmd"
+	"github.com/juju/gnuflag"
+	"github.com/juju/idmclient/params"
 	"gopkg.in/errgo.v1"
 )
 
 type findCommand struct {
-	userCommand
+	idmCommand
+
+	out cmd.Output
+
+	email             string
+	lastLoginDays     uint
+	lastDischargeDays uint
 }
 
 func newFindCommand() cmd.Command {
@@ -18,29 +26,65 @@ func newFindCommand() cmd.Command {
 }
 
 var findDoc = `
-The find command outputs the username associated with the specified user.
+The find command finds users that match the request parameters.
 
     user-admin find -e bob@example.com
+    user-admin find --last-login=30
 `
 
 func (c *findCommand) Info() *cmd.Info {
 	return &cmd.Info{
 		Name:    "find",
-		Args:    c.args(),
-		Purpose: "find a user",
+		Purpose: "find users",
 		Doc:     findDoc,
 	}
 }
 
+func (c *findCommand) SetFlags(f *gnuflag.FlagSet) {
+	c.idmCommand.SetFlags(f)
+
+	c.out.AddFlags(f, "smart", cmd.DefaultFormatters)
+
+	f.StringVar(&c.email, "e", "", "email address of the user")
+	f.StringVar(&c.email, "email", "", "")
+	f.UintVar(&c.lastLoginDays, "last-login", 0, "users whose last successful login was within this number of days")
+	f.UintVar(&c.lastDischargeDays, "last-discharge", 0, "users whose last successful discharge was within this number of days")
+}
+
 func (c *findCommand) Init(args []string) error {
-	return errgo.Mask(c.userCommand.Init(nil))
+	return errgo.Mask(c.idmCommand.Init(nil))
 }
 
 func (c *findCommand) Run(ctxt *cmd.Context) error {
-	username, err := c.lookupUser(ctxt)
+	client, err := c.idmCommand.Client(ctxt)
 	if err != nil {
 		return errgo.Mask(err)
 	}
-	_, err = fmt.Fprintln(ctxt.Stdout, username)
-	return errgo.Mask(err)
+	req := params.QueryUsersRequest{
+		Email: c.email,
+	}
+	if c.lastLoginDays > 0 {
+		req.LastLoginSince = daysAgo(c.lastLoginDays)
+	}
+	if c.lastDischargeDays > 0 {
+		req.LastDischargeSince = daysAgo(c.lastDischargeDays)
+	}
+	usernames, err := client.QueryUsers(&req)
+	if err != nil {
+		return errgo.Mask(err)
+	}
+	return c.out.Write(ctxt, usernames)
+}
+
+// daysAgo returns the current time less the given
+// number of days, formatted as a string as required
+// by time fields in params.QueryUsersRequest.
+func daysAgo(days uint) string {
+	t := time.Now().AddDate(0, 0, -int(days))
+	b, err := t.MarshalText()
+	if err != nil {
+		// This should be impossible unless things are severly wrong.
+		panic(err)
+	}
+	return string(b)
 }
