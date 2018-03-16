372d599131cccf213ef680af18206dd5ae872c99
Martin Hilton <martin.hilton@canonical.com>
2018-03-16 17:57:32 +0000

    cmd/migrate-db: Support migrating to SQL stores

---
+++ cmd/migrate-db/internal/legacy.go
@@ -0,0 +1,117 @@
+// Copyright 2018 Canonical Ltd.
+
+package internal
+
+import (
+	"log"
+	"strings"
+
+	errgo "gopkg.in/errgo.v1"
+	"gopkg.in/macaroon-bakery.v2/bakery"
+	mgo "gopkg.in/mgo.v2"
+
+	"github.com/CanonicalLtd/blues-identity/cmd/migrate-db/internal/mongodoc"
+	"github.com/CanonicalLtd/blues-identity/store"
+)
+
+// A LegacySource is a Source from a legacy mgo store.
+type LegacySource struct {
+	db       *mgo.Database
+	identity *store.Identity
+	iter     *mgo.Iter
+	err      error
+}
+
+// NewLegacySource creates a LegacySource from the given database.
+func NewLegacySource(db *mgo.Database) *LegacySource {
+	return &LegacySource{
+		db: db,
+	}
+}
+
+// Next implements Source.Next.
+func (s *LegacySource) Next() bool {
+	if s.iter == nil {
+		s.iter = s.db.C("identities").Find(nil).Iter()
+	}
+	for {
+		var doc mongodoc.Identity
+		if !s.iter.Next(&doc) {
+			return false
+		}
+		var err error
+		s.identity, err = convert(&doc)
+		if err != nil {
+			log.Printf("cannot convert identity (skipping): %s", err)
+			continue
+		}
+		return true
+	}
+}
+
+func convert(doc *mongodoc.Identity) (*store.Identity, error) {
+	providerID := providerID(doc)
+	if providerID == "" {
+		return nil, errgo.Newf("unrecognised external ID %q", doc.ExternalID)
+	}
+	identity := &store.Identity{
+		Username:   doc.Username,
+		ProviderID: providerID,
+		Name:       doc.FullName,
+		Email:      doc.Email,
+		Groups:     doc.Groups,
+	}
+	if doc.LastLogin != nil {
+		identity.LastLogin = *doc.LastLogin
+	}
+	if doc.LastDischarge != nil {
+		identity.LastDischarge = *doc.LastDischarge
+	}
+	for _, k := range doc.PublicKeys {
+		var key bakery.Key
+		copy(key[:], k.Key)
+		identity.PublicKeys = append(identity.PublicKeys, bakery.PublicKey{key})
+	}
+	if doc.Owner != "" {
+		if doc.Owner == "admin@idm" {
+			identity.ProviderInfo = map[string][]string{
+				"owner": {string(store.MakeProviderIdentity("idm", "admin@idm")), "admin@idm"},
+			}
+		} else {
+			return nil, errgo.Newf("unrecognised owner for %s (%q)", doc.Username, doc.Owner)
+		}
+	}
+	if len(doc.SSHKeys) > 0 {
+		identity.ExtraInfo = map[string][]string{
+			"sshkeys": doc.SSHKeys,
+		}
+	}
+	return identity, nil
+}
+
+func providerID(doc *mongodoc.Identity) store.ProviderIdentity {
+	if doc.ExternalID == "" {
+		return store.MakeProviderIdentity("idm", doc.Username)
+	}
+	if strings.HasPrefix(doc.ExternalID, "https://login.ubuntu.com/+id") {
+		return store.MakeProviderIdentity("usso", doc.ExternalID)
+	}
+	if strings.HasPrefix(doc.ExternalID, "openid-connect:") {
+		// The only currently used openid provider is azure
+		return store.MakeProviderIdentity("azure", strings.TrimPrefix(doc.ExternalID, "openid-connect:"))
+	}
+	if strings.HasPrefix(doc.ExternalID, "usso-openid:") {
+		return store.MakeProviderIdentity("usso_macaroon", strings.TrimPrefix(doc.ExternalID, "usso-openid:"))
+	}
+	return ""
+}
+
+// Identity implements Source.Identity.
+func (s *LegacySource) Identity() *store.Identity {
+	return s.identity
+}
+
+// Err implements Source.Err.
+func (s *LegacySource) Err() error {
+	return errgo.Mask(s.iter.Err())
+}
---
+++ cmd/migrate-db/internal/migrate.go
@@ -0,0 +1,114 @@
+// Copyright 2018 Canonical Ltd.
+
+package internal
+
+import (
+	"log"
+	"strings"
+
+	"golang.org/x/net/context"
+	errgo "gopkg.in/errgo.v1"
+
+	"github.com/CanonicalLtd/blues-identity/store"
+)
+
+// SplitStoreSpecification splits a store specification string as
+// supplied in the command line arguments into a type and address.
+func SplitStoreSpecification(s string) (type_, addr string) {
+	n := strings.IndexByte(s, ':')
+	if n < 0 {
+		return s, ""
+	}
+	return s[:n], s[n+1:]
+}
+
+// A Source is the interface that Copy uses to collect migrated
+// identitites from.
+type Source interface {
+	// Next fetches the next identity from the source. Next returns
+	// true if the identity was successfully fetched. If there are no
+	// more identities, or an error occurs then false is returned,
+	// the Err method can be used to determine which.
+	Next() bool
+
+	// Identity returns the current identity for the source. The
+	// pointer is only valid unti Next is called again.
+	Identity() *store.Identity
+
+	// Err returns any error received whilst getting identities.
+	Err() error
+}
+
+// Copy creates a new identity in dst for every identity retreived from src.
+func Copy(ctx context.Context, dst store.Store, src Source) error {
+	var failed bool
+	update := store.Update{
+		store.Username:      store.Set,
+		store.Name:          store.Set,
+		store.Email:         store.Set,
+		store.Groups:        store.Set,
+		store.PublicKeys:    store.Set,
+		store.LastLogin:     store.Set,
+		store.LastDischarge: store.Set,
+		store.ProviderInfo:  store.Set,
+		store.ExtraInfo:     store.Set,
+	}
+	for src.Next() {
+		identity := src.Identity()
+		// The ID field is store specific, so cannot be copied between them.
+		identity.ID = ""
+		err := dst.UpdateIdentity(ctx, identity, update)
+		if err != nil {
+			log.Printf("cannot update user %s: %s", identity.Username, err)
+			failed = true
+		}
+	}
+	if failed {
+		return errgo.Newf("some updates failed")
+	}
+	if err := src.Err(); err != nil {
+		return errgo.Notef(err, "cannot read identities")
+	}
+	return nil
+}
+
+// A StoreSource is a Source that wraps a store.Store.
+type StoreSource struct {
+	index      int
+	identities []store.Identity
+	err        error
+}
+
+// NewStoreSource creates a new StoreSource that use the given store for
+// its source of identities.
+func NewStoreSource(ctx context.Context, st store.Store) *StoreSource {
+	ctx, close := st.Context(ctx)
+	defer close()
+	identities, err := st.FindIdentities(ctx, nil, store.Filter{}, nil, 0, 0)
+	return &StoreSource{
+		identities: identities,
+		err:        err,
+	}
+}
+
+// Next implements Source.Next.
+func (s *StoreSource) Next() bool {
+	if s.err != nil {
+		return false
+	}
+	s.index++
+	if s.index > len(s.identities) {
+		return false
+	}
+	return true
+}
+
+// Identity implements Source.Identity.
+func (s *StoreSource) Identity() *store.Identity {
+	return &s.identities[s.index-1]
+}
+
+// Err implements Source.Err.
+func (s *StoreSource) Err() error {
+	return s.err
+}
