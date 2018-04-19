a220e765976b25486cc665e32e6804ae19a8770a
Roger Peppe <rogpeppe@gmail.com>
2018-04-19 13:32:51 +0100

    charm: implement postgres relation

--- charm/hooks/status.py
+++ charm/hooks/status.py
@@ -1,15 +1,18 @@
 from charmhelpers.core import hookenv
 
+databases = ['mongodb', 'postgres']
+
 
 def charm_status(msg):
     if not msg:
         return 'blocked', 'unknown error'
     if msg.find('storage') == -1:
         return 'blocked', msg
-    if hookenv.relation_ids('db'):
-        # We've got a mongodb relation, but we're still getting
-        # an error with "storage" in it. That's probably
-        # because the mongodb application at the other
-        # end of the relation hasn't set its attributes yet.
-        return 'waiting', 'waiting for db relation'
-    return 'blocked', 'need db relation'
+    for db in databases:
+        if hookenv.relation_ids(db):
+            # We've got a database relation, but we're still getting
+            # an error with "storage" in it. That's probably
+            # because the application at the other
+            # end of the relation hasn't set its attributes yet.
+            return 'waiting', 'waiting for {} relation'.format(db)
+    return 'blocked', 'need {} relation'.format(' or '.join(databases))
