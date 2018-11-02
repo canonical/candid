778148d61d984cd13da4c2ccdf4af9a4e2b53991
Roger Peppe <rogpeppe@gmail.com>
2018-11-02 19:05:09 +0000

    cmd/migrate-db/internal: use quicktest

--- cmd/migrate-db/internal/migrate_test.go
+++ cmd/migrate-db/internal/migrate_test.go
@@ -4,12 +4,11 @@
 package internal_test
 
 import (
+	"testing"
 	"time"
 
-	"github.com/juju/testing"
-	jc "github.com/juju/testing/checkers"
+	qt "github.com/frankban/quicktest"
 	"golang.org/x/net/context"
-	gc "gopkg.in/check.v1"
 	errgo "gopkg.in/errgo.v1"
 	"gopkg.in/macaroon-bakery.v2/bakery"
 
@@ -17,12 +16,6 @@ import (
 	"github.com/CanonicalLtd/candid/store"
 	"github.com/CanonicalLtd/candid/store/memstore"
 )
-
-type migrateSuite struct {
-	testing.IsolationSuite
-}
-
-var _ = gc.Suite(&migrateSuite{})
 
 var splitStoreSpecificationTests = []struct {
 	spec       string
@@ -50,16 +43,22 @@ var splitStoreSpecificationTests = []struct {
 	expectAddr: "",
 }}
 
-func (s *migrateSuite) TestSplitStoreSpecification(c *gc.C) {
+func TestSplitStoreSpecification(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
 	for i, test := range splitStoreSpecificationTests {
 		c.Logf("%d. %s", i, test.spec)
 		type_, addr := internal.SplitStoreSpecification(test.spec)
-		c.Assert(type_, gc.Equals, test.expectType)
-		c.Assert(addr, gc.Equals, test.expectAddr)
+		c.Assert(type_, qt.Equals, test.expectType)
+		c.Assert(addr, qt.Equals, test.expectAddr)
 	}
 }
 
-func (s *migrateSuite) TestStoreSource(c *gc.C) {
+func TestStoreSource(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
 	st := memstore.NewStore()
 	ctx := context.Background()
 	err := st.UpdateIdentity(ctx, &store.Identity{
@@ -68,38 +67,44 @@ func (s *migrateSuite) TestStoreSource(c *gc.C) {
 	}, store.Update{
 		store.Username: store.Set,
 	})
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 	err = st.UpdateIdentity(ctx, &store.Identity{
 		ProviderID: store.MakeProviderIdentity("test", "2"),
 		Username:   "test2",
 	}, store.Update{
 		store.Username: store.Set,
 	})
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 	source := internal.NewStoreSource(ctx, st)
 	usernames := make(map[string]struct{})
 	for source.Next() {
 		usernames[source.Identity().Username] = struct{}{}
 	}
-	c.Assert(source.Err(), gc.Equals, nil)
-	c.Assert(usernames, jc.DeepEquals, map[string]struct{}{
+	c.Assert(source.Err(), qt.Equals, nil)
+	c.Assert(usernames, qt.DeepEquals, map[string]struct{}{
 		"test1": {},
 		"test2": {},
 	})
 }
 
-func (s *migrateSuite) TestStoreSourceEmpty(c *gc.C) {
+func TestStoreSourceEmpty(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
 	store := memstore.NewStore()
 	source := internal.NewStoreSource(context.Background(), store)
-	c.Assert(source.Next(), gc.Equals, false)
-	c.Assert(source.Err(), gc.Equals, nil)
+	c.Assert(source.Next(), qt.Equals, false)
+	c.Assert(source.Err(), qt.Equals, nil)
 }
 
-func (s *migrateSuite) TestStoreSourceError(c *gc.C) {
+func TestStoreSourceError(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
 	testError := errgo.New("test error")
 	source := internal.NewStoreSource(context.Background(), errorStore{testError})
-	c.Assert(source.Next(), gc.Equals, false)
-	c.Assert(source.Err(), gc.Equals, testError)
+	c.Assert(source.Next(), qt.Equals, false)
+	c.Assert(source.Err(), qt.Equals, testError)
 }
 
 type errorStore struct {
@@ -126,7 +131,10 @@ func (s errorStore) IdentityCounts(_ context.Context) (map[string]int, error) {
 	return nil, s.err
 }
 
-func (s *migrateSuite) TestCopy(c *gc.C) {
+func TestCopy(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
 	store1 := memstore.NewStore()
 	ctx := context.Background()
 	k1 := bakery.MustGenerateKey()
@@ -157,7 +165,7 @@ func (s *migrateSuite) TestCopy(c *gc.C) {
 		store.ProviderInfo:  store.Set,
 		store.ExtraInfo:     store.Set,
 	})
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 
 	identity2 := store.Identity{
 		ProviderID: store.MakeProviderIdentity("test", "2"),
@@ -166,29 +174,29 @@ func (s *migrateSuite) TestCopy(c *gc.C) {
 	err = store1.UpdateIdentity(ctx, &identity2, store.Update{
 		store.Username: store.Set,
 	})
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 
 	store2 := memstore.NewStore()
 	err = internal.Copy(ctx, store2, internal.NewStoreSource(ctx, store1))
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 
 	copiedIdentity1 := store.Identity{
 		ProviderID: store.MakeProviderIdentity("test", "1"),
 	}
 	err = store2.Identity(ctx, &copiedIdentity1)
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 	normalize(&identity1)
 	normalize(&copiedIdentity1)
-	c.Assert(copiedIdentity1, jc.DeepEquals, identity1)
+	c.Assert(copiedIdentity1, qt.DeepEquals, identity1)
 
 	copiedIdentity2 := store.Identity{
 		ProviderID: store.MakeProviderIdentity("test", "2"),
 	}
 	err = store2.Identity(ctx, &copiedIdentity2)
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 	normalize(&identity2)
 	normalize(&copiedIdentity2)
-	c.Assert(copiedIdentity2, jc.DeepEquals, identity2)
+	c.Assert(copiedIdentity2, qt.DeepEquals, identity2)
 }
 
 func normalize(identity *store.Identity) {
@@ -207,16 +215,22 @@ func normalize(identity *store.Identity) {
 	}
 }
 
-func (s *migrateSuite) TestCopySrcError(c *gc.C) {
+func TestCopySrcError(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
 	store1 := errorStore{errgo.New("test error")}
 	ctx := context.Background()
 
 	store2 := memstore.NewStore()
 	err := internal.Copy(ctx, store2, internal.NewStoreSource(ctx, store1))
-	c.Assert(err, gc.ErrorMatches, "cannot read identities: test error")
+	c.Assert(err, qt.ErrorMatches, "cannot read identities: test error")
 }
 
-func (s *migrateSuite) TestCopyDstError(c *gc.C) {
+func TestCopyDstError(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
 	store1 := memstore.NewStore()
 	ctx := context.Background()
 	k1 := bakery.MustGenerateKey()
@@ -247,9 +261,9 @@ func (s *migrateSuite) TestCopyDstError(c *gc.C) {
 		store.ProviderInfo:  store.Set,
 		store.ExtraInfo:     store.Set,
 	})
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 
 	store2 := errorStore{errgo.New("test error")}
 	err = internal.Copy(ctx, store2, internal.NewStoreSource(ctx, store1))
-	c.Assert(err, gc.ErrorMatches, "some updates failed")
+	c.Assert(err, qt.ErrorMatches, "some updates failed")
 }
