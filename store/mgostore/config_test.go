9793d015747efa18dc7e1c906c2ed38f59555da8
Roger Peppe <rogpeppe@gmail.com>
2018-11-02 11:48:59 +0000

    store/mgostore: use quicktest

--- store/mgostore/config_test.go
+++ store/mgostore/config_test.go
@@ -1,54 +1,33 @@
 package mgostore_test
 
 import (
-	"os"
+	"testing"
 
-	"github.com/juju/mgotest"
-	gc "gopkg.in/check.v1"
-	errgo "gopkg.in/errgo.v1"
+	qt "github.com/frankban/quicktest"
 	"gopkg.in/yaml.v2"
 
 	"github.com/CanonicalLtd/candid/store"
+	"github.com/CanonicalLtd/candid/store/storetest"
 	"github.com/CanonicalLtd/candid/store/mgostore"
-	storetesting "github.com/CanonicalLtd/candid/store/testing"
 )
 
-var _ = gc.Suite(&configSuite{})
+func TestUnmarshal(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
 
-type configSuite struct {
-	db  *mgotest.Database
-	url string
-}
-
-func (s *configSuite) SetUpTest(c *gc.C) {
-	var err error
-	s.db, err = mgotest.New()
-	if errgo.Cause(err) == mgotest.ErrDisabled {
-		c.Skip("mgotest disabled")
-	}
-	c.Assert(err, gc.Equals, nil)
-	s.url = os.Getenv("MGOCONNECTIONSTRING")
-	if s.url == "" {
-		s.url = "localhost"
-	}
-}
-
-func (s *configSuite) TearDownTest(c *gc.C) {
-	if s.db != nil {
-		s.db.Close()
-	}
-}
-
-func (s *configSuite) TestUnmarshal(c *gc.C) {
-	storetesting.TestUnmarshal(c, `
+	f := newFixture(c)
+	storetest.TestUnmarshal(c, `
 storage:
     type: mongodb
-    address: `+s.url+`
-    database: `+s.db.Name+`
+    address: `+f.connStr+`
+    database: `+f.db.Name+`
 `)
 }
 
-func (s *configSuite) TestUnmarshalWithNoAddress(c *gc.C) {
+func TestUnmarshalWithNoAddress(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
 	configData := `
 storage:
     type: mongodb
@@ -57,22 +36,26 @@ storage:
 		Storage *store.Config `yaml:"storage"`
 	}
 	err := yaml.Unmarshal([]byte(configData), &cfg)
-	c.Assert(err, gc.ErrorMatches, `cannot unmarshal mongodb configuration: no address field in mongodb storage configuration`)
+	c.Assert(err, qt.ErrorMatches, `cannot unmarshal mongodb configuration: no address field in mongodb storage configuration`)
 }
 
-func (s *configSuite) TestUnmarshalWithoutDatabase(c *gc.C) {
+func TestUnmarshalWithoutDatabase(t *testing.T) {
+	c := qt.New(t)
+	defer c.Done()
+
+	f := newFixture(c)
 	configData := `
 storage:
     type: mongodb
-    address: ` + s.url + `
+    address: ` + f.connStr + `
 `
 	var cfg struct {
 		Storage *store.Config `yaml:"storage"`
 	}
 	err := yaml.Unmarshal([]byte(configData), &cfg)
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 
 	p, ok := cfg.Storage.BackendFactory.(mgostore.Params)
-	c.Assert(ok, gc.Equals, true)
-	c.Assert(p.Database, gc.Equals, "candid")
+	c.Assert(ok, qt.Equals, true)
+	c.Assert(p.Database, qt.Equals, "candid")
 }
