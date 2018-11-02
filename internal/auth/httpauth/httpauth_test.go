cf363272e2e8294fe9b359141013db040b249bee
Roger Peppe <rogpeppe@gmail.com>
2018-11-02 18:30:31 +0000

    internal/auth/httpauth: use quicktest

--- internal/auth/httpauth/httpauth_test.go
+++ internal/auth/httpauth/httpauth_test.go
@@ -6,11 +6,13 @@ package httpauth_test
 import (
 	"encoding/base64"
 	"net/http"
+	"testing"
 
+	qt "github.com/frankban/quicktest"
+	"github.com/frankban/quicktest/qtsuite"
 	"github.com/juju/aclstore/v2"
 	"golang.org/x/net/context"
 	"gopkg.in/CanonicalLtd/candidclient.v1/params"
-	gc "gopkg.in/check.v1"
 	"gopkg.in/errgo.v1"
 	"gopkg.in/macaroon-bakery.v2/bakery"
 	"gopkg.in/macaroon-bakery.v2/bakery/identchecker"
@@ -18,23 +20,26 @@ import (
 
 	"github.com/CanonicalLtd/candid/internal/auth"
 	"github.com/CanonicalLtd/candid/internal/auth/httpauth"
-	"github.com/CanonicalLtd/candid/internal/candidtest"
+	candidtest "github.com/CanonicalLtd/candid/internal/qtcandidtest"
 )
 
 type authSuite struct {
-	candidtest.StoreSuite
+	store      *candidtest.Store
 	oven       *bakery.Oven
 	aclManager *aclstore.Manager
 }
 
-var _ = gc.Suite(&authSuite{})
+func TestAuth(t *testing.T) {
+	qtsuite.Run(qt.New(t), &authSuite{})
+}
 
 const identityLocation = "https://identity.test/id"
 
-func (s *authSuite) SetUpTest(c *gc.C) {
-	s.StoreSuite.SetUpTest(c)
+func (s *authSuite) Init(c *qt.C) {
+	s.store = candidtest.NewStore()
+
 	key, err := bakery.GenerateKey()
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 	locator := bakery.NewThirdPartyStore()
 	locator.AddInfo(identityLocation, bakery.ThirdPartyInfo{
 		PublicKey: key.Public,
@@ -46,17 +51,13 @@ func (s *authSuite) SetUpTest(c *gc.C) {
 		Location: "identity",
 	})
 	s.aclManager, err = aclstore.NewManager(context.Background(), aclstore.Params{
-		Store:             s.ACLStore,
+		Store:             s.store.ACLStore,
 		InitialAdminUsers: []string{auth.AdminUsername},
 	})
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 }
 
-func (s *authSuite) TearDownTest(c *gc.C) {
-	s.StoreSuite.TearDownTest(c)
-}
-
-func (s *authSuite) TestAuthorizeWithAdminCredentials(c *gc.C) {
+func (s *authSuite) TestAuthorizeWithAdminCredentials(c *qt.C) {
 	tests := []struct {
 		about              string
 		adminPassword      string
@@ -90,50 +91,53 @@ func (s *authSuite) TestAuthorizeWithAdminCredentials(c *gc.C) {
 		},
 		expectErrorMessage: "could not determine identity: invalid credentials",
 	}}
-	for i, test := range tests {
-		c.Logf("test %d. %s", i, test.about)
-		authorizer, err := auth.New(auth.Params{
-			AdminPassword:    test.adminPassword,
-			Location:         identityLocation,
-			Store:            s.Store,
-			MacaroonVerifier: s.oven,
-			ACLManager:       s.aclManager,
+	for _, test := range tests {
+		c.Run(test.about, func(c *qt.C) {
+			authorizer, err := auth.New(auth.Params{
+				AdminPassword:    test.adminPassword,
+				Location:         identityLocation,
+				Store:            s.store.Store,
+				MacaroonVerifier: s.oven,
+				ACLManager:       s.aclManager,
+			})
+			httpAuthorizer := httpauth.New(s.oven, authorizer)
+			req, _ := http.NewRequest("GET", "/", nil)
+			for attr, val := range test.header {
+				req.Header[attr] = val
+			}
+			authInfo, err := httpAuthorizer.Auth(context.Background(), req, identchecker.LoginOp)
+			if test.expectErrorMessage != "" {
+				c.Assert(err, qt.ErrorMatches, test.expectErrorMessage)
+				c.Assert(errgo.Cause(err), qt.Equals, params.ErrUnauthorized)
+				return
+			}
+			c.Assert(err, qt.Equals, nil)
+			c.Assert(authInfo.Identity.Id(), qt.Equals, auth.AdminUsername)
 		})
-		httpAuthorizer := httpauth.New(s.oven, authorizer)
-		req, _ := http.NewRequest("GET", "/", nil)
-		for attr, val := range test.header {
-			req.Header[attr] = val
-		}
-		authInfo, err := httpAuthorizer.Auth(context.Background(), req, identchecker.LoginOp)
-		if test.expectErrorMessage != "" {
-			c.Assert(err, gc.ErrorMatches, test.expectErrorMessage)
-			c.Assert(errgo.Cause(err), gc.Equals, params.ErrUnauthorized)
-			continue
-		}
-		c.Assert(err, gc.Equals, nil)
-		c.Assert(authInfo.Identity.Id(), gc.Equals, auth.AdminUsername)
 	}
 }
 
-func (s *authSuite) TestAuthorizeMacaroonRequired(c *gc.C) {
+func (s *authSuite) TestAuthorizeMacaroonRequired(c *qt.C) {
 	authorizer, err := auth.New(auth.Params{
 		AdminPassword:    "open sesame",
 		Location:         identityLocation,
-		Store:            s.Store,
+		Store:            s.store.Store,
 		MacaroonVerifier: s.oven,
 		ACLManager:       s.aclManager,
 	})
 	httpAuthorizer := httpauth.New(s.oven, authorizer)
 	req, err := http.NewRequest("GET", "http://example.com/v1/test", nil)
-	c.Assert(err, gc.Equals, nil)
+	c.Assert(err, qt.Equals, nil)
 	authInfo, err := httpAuthorizer.Auth(context.Background(), req, identchecker.LoginOp)
-	c.Assert(err, gc.ErrorMatches, `macaroon discharge required: authentication required`)
-	c.Assert(authInfo, gc.IsNil)
-	c.Assert(errgo.Cause(err), gc.FitsTypeOf, (*httpbakery.Error)(nil))
-	derr := errgo.Cause(err).(*httpbakery.Error)
-	c.Assert(derr.Info.CookieNameSuffix, gc.Equals, "candid")
-	c.Assert(derr.Info.MacaroonPath, gc.Equals, "../")
-	c.Assert(derr.Info.Macaroon, gc.NotNil)
+	c.Assert(err, qt.ErrorMatches, `macaroon discharge required: authentication required`)
+	c.Assert(authInfo, qt.IsNil)
+	derr, ok := errgo.Cause(err).(*httpbakery.Error)
+	if !ok {
+		c.Fatalf("error %#v is not httpbakery.Error", err)
+	}
+	c.Assert(derr.Info.CookieNameSuffix, qt.Equals, "candid")
+	c.Assert(derr.Info.MacaroonPath, qt.Equals, "../")
+	c.Assert(derr.Info.Macaroon, qt.Not(qt.IsNil))
 }
 
 func b64str(s string) string {
